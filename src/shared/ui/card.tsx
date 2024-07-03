f7acfd7c3de4a2f0d29fd8c253bfdcf1aaed4262
gleb <glen2004111@gmial.com>
2024-07-03 14:26:15 +0300

    first commit

--- src/shared/ui/card.tsx
+++ src/shared/ui/card.tsx
@@ -1,7 +1,7 @@
 import { ICard } from "@/entities/character/api/type";
 import React, { ReactElement, ReactNode } from "react";
 
-const Card = ({ children }: ICard, props: any) => {
+const Card = ({ children }: ICard) => {
   return (
     <div className="flex flex-col px-4 py-2 shadow-xl rounded-sm w-64 hover:shadow-2xl hover:shadow-green-500 hover:cursor-pointer">
       {children}
